6d5514f9b45a602b2f921d9217e54b17b3500fb2
mokle <moklesur508@gmail.com>
2017-04-26 04:07:37 +0600

    New

--- app/src/main/java/com/example/mokle/volleyjsonpass/MainActivity.java
+++ app/src/main/java/com/example/mokle/volleyjsonpass/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.mokle.volleyjsonpass;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     Button getInfoBtn;
     TextView nametxt, emailtxt, mobiletxt;
+    String jsonURL =  "http://192.168.0.105/getinfo.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +33,17 @@ public class MainActivity extends AppCompatActivity {
         getInfoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                JsonObjectRequest jRequest = new JsonObjectRequest(Request.Method.POST, jsonURL, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
             }
         });
     }
